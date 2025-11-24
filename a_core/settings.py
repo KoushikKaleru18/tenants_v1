a44dedc84c10206f92742a58fdaaf9b6f7d151ea
rick <kalerukoushik18@gmail.com>
2025-11-24 21:25:00 +0530

    Lesson 1 - Base Project with 'Item' Model

--- a_core/settings.py
+++ a_core/settings.py
@@ -103,8 +103,12 @@ WSGI_APPLICATION = 'a_core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres_v1',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
--- a_home/urls.py
+++ a_home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from a_home.views import *
 
 urlpatterns = [
-    path('', home_view, name="home"),  
+    path('', home_view, name="home"),
+    path('create-item/', create_item, name="create-item"),
 ]
