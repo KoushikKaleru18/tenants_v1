7afe85119aa523e656ce5049b7c4ef7363c73c30
rick <kalerukoushik18@gmail.com>
2025-11-26 23:13:41 +0530

    test

--- a_core/settings.py
+++ a_core/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
-
+import os
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
